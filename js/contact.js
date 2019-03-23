e798fc1d09ba94ecf8003253105d8414fee45af7
Fernando Bellido <fbellidopazos@gmail.com>
2019-03-23 21:54:19 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
                 },
                 message: {
                     required: true,
-                    minlength: 10
+                    minlength: 5
                 }
             },
             messages: {
